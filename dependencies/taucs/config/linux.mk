2bda750cddbf2320656e817d6ec6f6fa6244b7e9
Yichao Zhou <broken.zhoug@gmail.com>
2016-09-09 17:17:00 -0700

    cleanup for compile on my own laptop

--- dependencies/taucs/config/linux.mk
+++ dependencies/taucs/config/linux.mk
@@ -32,24 +32,10 @@ AOUTFLG   =
 RANLIB    = ranlib
 RM        = rm -rf
 
-# These are for a Pentium4 version of ATLAS (not bundled with taucs)
-#LIBBLAS   = -L /home/stoledo/Public/Linux_P4SSE2/lib -lf77blas -lcblas -latlas \
-#            -L /usr/lib/gcc-lib/i386-redhat-linux/2.96 -lg2c
-#LIBLAPACK = -L /home/stoledo/Public/Linux_P4SSE2/lib -llapack
-
-LIBBLAS   = -L/usr/lib64/atlas -L/usr/lib64/atlas-sse3 -lf77blas -lcblas -latlas
+LIBBLAS   = -lblas
 LIBLAPACK = -llapack
 
 LIBMETIS  = -lmetis
 
 LIBF77 = -lgfortran
 LIBC   = -lm 
-
-#########################################################
-
-
-
-
-
-
-
